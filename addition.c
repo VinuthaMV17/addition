bfea35c604854fb01c6153f12b9d5742b8b3f1ef
VinuthaMV17 <vinuthavinutha319@gmail.com>
2025-03-26 10:08:12 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ int add(int n1, int n2)
+ {
+     return n1+n2;
+ }
+ int main() 
+ {
+     int n1, n2,result;
+     printf("Enter the first number: ");
+     scanf("%d", &n1);
+ 
+     printf("Enter the second number: ");
+     scanf("%d", &n2);
+ 
+     result = add(n1, n2);
+     printf("The sum of %d and %d is: %d\n", n1, n2, result);
+  return 0;
+ }
